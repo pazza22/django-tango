9b8600ef206adfe79bb646df7eecaea30c1ac645
Praneeth <developer.praneethrk@gmail.com>
2018-06-09 23:53:44 +0530

    Add db

--- music/urls.py
+++ music/urls.py
@@ -4,5 +4,9 @@ from . import views
 from django.conf.urls import url
 
 urlpatterns = [
+    # /music/
     url(r'^$', views.index, name='index'),
+
+    # /music/232/
+    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
 ]
