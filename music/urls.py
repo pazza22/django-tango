f3c08e2f7b8173f64ba8a170619b30d62cf51d1f
Praneeth <developer.praneethrk@gmail.com>
2018-06-13 12:02:08 +0530

    Add Forms

--- music/urls.py
+++ music/urls.py
@@ -3,10 +3,15 @@ from django.urls import path
 from . import views
 from django.conf.urls import url
 
+app_name = 'music'
+
 urlpatterns = [
     # /music/
     url(r'^$', views.index, name='index'),
 
     # /music/232/
     url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
+
+    # /music/232/favorite
+    url(r'^(?P<album_id>[0-9]+)/favorite/$', views.favorite, name='favorite'),
 ]
