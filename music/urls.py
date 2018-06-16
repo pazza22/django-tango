bfe201b5bd485b55f63f34cd371f40b94239e06f
Praneeth <developer.praneethrk@gmail.com>
2018-06-17 00:42:34 +0530

    Added Upload Functionality

--- music/urls.py
+++ music/urls.py
@@ -15,4 +15,10 @@ urlpatterns = [
     # /music/album/add
     url(r'album/add/$', views.AlbumCreate.as_view(), name='album-add'),
 
+    # /music/album/[updateid]
+    url(r'album/(?P<pk>[0-9]+)/$', views.AlbumUpdate.as_view(), name='album-update'),
+
+    # /music/album/[deleteid]/delete
+    url(r'album/(?P<pk>[0-9]+)/delete/$', views.AlbumDelete.as_view(), name='album-delete'),
+
 ]
