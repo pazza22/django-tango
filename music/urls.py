974ace4382a899cd260421994b53b1799578ef60
Praneeth <developer.praneethrk@gmail.com>
2018-06-16 14:27:39 +0530

    Add View Templates

--- music/urls.py
+++ music/urls.py
@@ -7,11 +7,9 @@ app_name = 'music'
 
 urlpatterns = [
     # /music/
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
 
     # /music/232/
-    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
 
-    # /music/232/favorite
-    url(r'^(?P<album_id>[0-9]+)/favorite/$', views.favorite, name='favorite'),
 ]
