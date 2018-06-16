3431f4dd2a9816d4562d5673507703c00bceab3a
Praneeth <developer.praneethrk@gmail.com>
2018-06-16 21:20:49 +0530

    ModelForm Create View

--- music/urls.py
+++ music/urls.py
@@ -12,4 +12,7 @@ urlpatterns = [
     # /music/232/
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
 
+    # /music/album/add
+    url(r'album/add/$', views.AlbumCreate.as_view(), name='album-add'),
+
 ]
